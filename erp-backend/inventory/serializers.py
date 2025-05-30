2df1dc56c1111c6a82030db003b046fe43d7c246
Daidan Lu <dlu59@wisc.edu>
2025-05-29 19:38:33 -0500

    Add order models and serializers; switch to PostgreSQL

--- erp-backend/inventory/serializers.py
+++ erp-backend/inventory/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Order, OrderItem
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer_name', 'created_at', 'items']
